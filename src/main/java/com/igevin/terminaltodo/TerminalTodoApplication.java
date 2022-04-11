5f4a59ffb1293e1f39ea4927e53b9ce8fbdb0f3f
gevin <flyhigher139@gmail.com>
2022-04-11 14:27:54 +0800

    init

---
+++ src/main/java/com/igevin/terminaltodo/TerminalTodoApplication.java
@@ -0,0 +1,13 @@
+package com.igevin.terminaltodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerminalTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igevin/terminaltodo/TerminalTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igevin.terminaltodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
